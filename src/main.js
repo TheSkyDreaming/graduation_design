510e60101db3d2e059f1daf59570eacab4090f69
TheSkyDreaming <2446031697@qq.com>
2020-02-24 15:14:47 +0800

    install axios and config axios in main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
 // 本文件是项目打包的入口文件。
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
 new Vue({
   router,
   vuetify,
